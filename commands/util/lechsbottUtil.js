ed3e95231eb4191e8d1dc62049a8d191ed621568
Lechixy <51778131+Lechixy@users.noreply.github.com>
2021-06-13 21:31:55 +0300

    Update lechsbottUtil.js

--- commands/util/lechsbottUtil.js
+++ commands/util/lechsbottUtil.js
@@ -1,14 +1,14 @@
-let config;
+//let config;
   
-  try {
-    config = require("/Users/melih/Desktop/lechsbott/config.json");
-  } catch (error) {
-    config = null;
-  }
+//  try {
+//    config = require("/Users/melih/Desktop/lechsbott/config.json");
+//  } catch (error) {
+//    config = null;
+//  }
   
-exports.YOUTUBE_API_KEY = config.YOUTUBE_API_KEY;
-exports.SOUNDCLOUD_CLIENT_ID = config.SOUNDCLOUD_CLIENT_ID;
-exports.DEFAULT_VOLUME = config.DEFAULT_VOLUME;
-exports.LECHSBOTTKEY = config.LECHSBOTTKEY;
-exports.OWNER1 = config.OWNER1;
-exports.OWNER2 = config.OWNER2;
+//exports.YOUTUBE_API_KEY = config.YOUTUBE_API_KEY;
+//exports.SOUNDCLOUD_CLIENT_ID = config.SOUNDCLOUD_CLIENT_ID;
+//exports.DEFAULT_VOLUME = config.DEFAULT_VOLUME;
+//exports.LECHSBOTTKEY = config.LECHSBOTTKEY;
+//exports.OWNER1 = config.OWNER1;
+//exports.OWNER2 = config.OWNER2;
