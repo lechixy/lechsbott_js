a23bcb6369b6c7d8461653885e125c4e3f18ab76
Lechixy <51778131+Lechixy@users.noreply.github.com>
2021-06-13 21:16:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
+const client = new Discord.Client({
+    restTimeOffset: 0
+});
 const dotenv = require('dotenv')
 
 const memberCounter = require('./commands/counters/member-counter')
+const { LECHSBOTTKEY } = require("./commands/util/lechsbottUtil");
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
@@ -28,4 +31,4 @@ client.on('ready', () => {
 
 });
 
-client.login('NzUzOTA2ODc0NzI5ODg5ODUz.X1tAfQ.G-PKpPkxl9D7uVEkNigw9zfXZsM');
+client.login(LECHSBOTTKEY);
