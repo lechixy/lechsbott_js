4aea90739972f3c2b2ff665b68ead414713258b9
Lechixy <51778131+Lechixy@users.noreply.github.com>
2021-06-22 13:58:21 +0300

    Delete commands directory

--- commands/configtest.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'configtest',
-    description: 'asdagda',
-    async execute(client, message, args, cmd, Discord) {
-        const { YOUTUBE_API_KEY, SOUNDCLOUD_CLIENT_ID, DEFAULT_VOLUME } = require("../util/lechsbottUtil");
-
-        message.channel.send(YOUTUBE_API_KEY)
-    }
-}
