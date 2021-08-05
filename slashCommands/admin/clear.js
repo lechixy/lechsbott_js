7a79c671ee267ede8ddd75bfca7bb52f77acae11
Lechixy <51778131+Lechixy@users.noreply.github.com>
2021-08-05 23:44:43 +0300

    Add files via upload

---
+++ slashCommands/admin/clear.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'clear',
+    description: 'Need to bulk delete messages, just use this command!',
+    options: [
+        {
+            type: "INTEGER",
+            name: "number",
+            description: "Choose an amount for clear messages, maximum 100 message will be deletable",
+            required: true
+          }
+    ],
+    async execute(client, interaction, args, Discord) {
+        
+        
+        
+        
+ }
+}
