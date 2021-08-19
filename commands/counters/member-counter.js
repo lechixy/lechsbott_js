1efe23a66ba62ad6c4421857be22583a00dd6b83
Lechixy <51778131+Lechixy@users.noreply.github.com>
2021-08-19 13:23:22 +0300

    Delete commands directory

--- commands/counters/member-counter.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (client, message, args) =>{
-    const guild = client.guilds.cache.get('840543133007609907');
-    setInterval(() =>{
-        const memberCount = guild.memberCount;
-        const channel = guild.channels.cache.get('840590912173506620');
-        channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
-    }, 5000);
-}
