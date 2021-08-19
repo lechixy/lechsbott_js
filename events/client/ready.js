9a85f6d78fa153203c13dbd61b7b8d5610003927
Lechixy <51778131+Lechixy@users.noreply.github.com>
2021-08-19 13:21:42 +0300

    Delete events directory

--- events/client/ready.js
+++
@@ -1,6 +0,0 @@
-module.exports = (Discord, client) => {
-    console.clear()
-    console.log('Lechsbott now online!')
-    console.log(`Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers.`);
-}
-
--- events/guild/guildCreate.js
+++
@@ -1,8 +0,0 @@
-module.exports = (Discord, client, guild) => {
-    let joinedembed = new Discord.MessageEmbed()
-    .setAuthor(`lechsbott`, `${client.user.displayAvatarURL()}`)
-    .setTitle(`Hey <@${guild.owner.id}>`)
-    .setDescription(`Joined to **${guild.name}**!\nIf you need anything about **lechsbott** just type \`l!help\` to guild chat!`)
-    .setTimestamp()
-    guild.owner.send(joinedembed)
-}
