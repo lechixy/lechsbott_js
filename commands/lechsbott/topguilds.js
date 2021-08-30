fbcdfb66b605c7114c96af9d298ca48279f19e9f
Lechixy <51778131+Lechixy@users.noreply.github.com>
2021-08-30 03:04:31 +0300

    Add files via upload

---
+++ commands/lechsbott/topguilds.js
@@ -0,0 +1,24 @@
+const { PREFIX } = require('../util/lechsbottUtil')
+
+
+module.exports = {
+    name:'topguilds',
+    aliases:['clientguilds'],
+    description:'',
+    async execute(client, message, args, cmd, Discord) {
+      
+        const guilds = client.guilds.cache.sort((a, b) => b.memberCount - a.memberCount).first(10);
+        
+        const description = guilds
+            .map((guild, index) => {
+            return `**${index+1} |** ${guild.name} **•** ${guild.memberCount} members`;
+            })
+            .join("\n")
+        
+        const embed = new Discord.MessageEmbed()
+        .setTitle('Top Guilds')
+        .setDescription(description)
+        message.channel.send({ embeds: [embed] });
+    
+  }
+}
