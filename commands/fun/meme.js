6702d3caf477e5b6148e7e0f37d69cb06af8d4ac
Lechixy <51778131+Lechixy@users.noreply.github.com>
2021-09-03 04:17:47 +0300

    Add files via upload

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -1,32 +1,56 @@
-const got = require('got');
+const fetch = require('node-fetch');
 
 module.exports = {
-    name:'meme',
-    aliases:['redditmeme'],
+    name: 'meme',
+    aliases: ['redditmeme'],
     cooldown: 5,
-    description:'',
+    description: '',
     async execute(client, message, args, cmd, Discord) {
-        
-        got('https://reddit.com/r/meme/random/.json').then(res => {
-            let content = JSON.parse(res.body);
 
-            let permalink = content[0].data.children[0].data.permalink;
-            let title = content[0].data.children[0].data.title;
-            let titleURL = `https://reddit.com${permalink}`
-            let meme = content[0].data.children[0].data.url;
-            let ups = content[0].data.children[0].data.ups;
-            let comments = content[0].data.children[0].data.num_comments;
-            let from = content[0].data.children[0].data.subreddit_name_prefixed;
+        const Reds = [
+            "memes",
+            "me_irl",
+            "dankmemes",
+            "Animemes",
+            "AdviceAnimals",
+            "MemeEconomy",
+            "ComedyCemetery"
 
-            const embed = new Discord.MessageEmbed()
-            .setTitle(title)
-            .setURL(titleURL)
-            .setColor('RANDOM')
-            .setImage(meme)
-            .setFooter(`👍 ${ups} 💬 ${comments} • ${from}`)
-            message.channel.send({ embeds: [embed] });
+        ];
+        const Rads = Reds[Math.floor(Math.random() * Reds.length)];
+
+        function getMeme(){
+            fetch(`https://reddit.com/r/${Rads}/random/.json`).then(async resp => {
+            const json = await resp.json();
+            
+            if(json[0].data.children[0].data.is_video === true){
+                return getMeme()
+            }
+
+            try {
+                let permalink = json[0].data.children[0].data.permalink;
+                let title = json[0].data.children[0].data.title;
+                let titleURL = `https://reddit.com${permalink}`
+                let meme = json[0].data.children[0].data.url;
+                let ups = json[0].data.children[0].data.ups;
+                let comments = json[0].data.children[0].data.num_comments;
+                let from = json[0].data.children[0].data.subreddit_name_prefixed;
+    
+                const embed = new Discord.MessageEmbed()
+                .setTitle(title)
+                .setURL(titleURL)
+                .setColor('RANDOM')
+                .setImage(meme)
+                .setFooter(`👍 ${ups} 💬 ${comments} • ${from}`)
+                message.channel.send({ embeds: [embed] });
+            } catch (err) {
+                console.log(err);
+            }
+
         })
+        }
+
+        getMeme()
         
-        
-  }
+    }
 }
