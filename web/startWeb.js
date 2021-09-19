d62e678acde7bd359f9576f4d82df3f6cc24a520
Lechixy <51778131+Lechixy@users.noreply.github.com>
2021-09-19 19:28:08 +0300

    Add files via upload

--- web/startWeb.js
+++ web/startWeb.js
@@ -4,16 +4,25 @@ function startWeb(){
   const express = require("express")
   const app = express()
   const ejs = require('ejs')
+  const settings = require("./settings.json")
   const server = http.createServer(app)
+
   app.set('view engine', 'ejs')
   app.use(express.static("public"))
 
   app.get("/", (req, res) => {
-    res.render("index")
+    res.render("index", { bot: settings.website })
   })
 
+  app.get("/commands", (req, res) => {
+    res.render("commands", { bot: settings.website })
+  })
+
+  app.all('*', (req, res) => res.render('errors/404'));
+
+  
   const listener = server.listen(8000, function(){
-    console.log(`Your app is listening on port ${listener.address().port}`)
+    console.log(`Your app is listening ${listener.address().address} on port ${listener.address().port}`)
   })
 
 }
