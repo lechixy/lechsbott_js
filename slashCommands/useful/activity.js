041fd07cd6ea83afb216e8529b02f19b5d9c49db
Lechixy <51778131+Lechixy@users.noreply.github.com>
2021-10-31 23:10:33 +0300

    Add files via upload

--- slashCommands/useful/activity.js
+++ slashCommands/useful/activity.js
@@ -1,202 +1,139 @@
 const fetch = require("node-fetch")
 const { LECHSBOTTKEY } = require("../../commands/util/lechsbottUtil")
+const { SlashCommand } = require('../../lechs_modules/SlashCommand/index')
 
-module.exports = {
+module.exports = new SlashCommand({
     name: 'activity',
     description: 'Use discords new voice channel activities!',
     options: [
         {
             name: 'which',
-            description: 'Which activity you want to Click to start?',
+            description: 'Which activity you want to start?',
             required: true,
             type: 'STRING',
             choices: [
                 {
-                    name: '💖 Youtube Together',
-                    value: 'Youtube Together',
+                    name: 'Youtube',
+                    value: 'yt',
                 },
                 {
-                    name: '🃏 Poker Night',
-                    value: 'Poker Night',
+                    name: 'Youtube Dev',
+                    value: 'ytdev',
                 },
                 {
-                    name: '🔪 Betrayal.io',
-                    value: 'Betrayal.io',
+                    name: 'Poker Night',
+                    value: 'poker',
                 },
                 {
-                    name: '🐟 Fishington.io',
-                    value: 'Fishington.io'
-                }
+                    name: 'Chess In The Park',
+                    value: 'chess',
+                },
+                {
+                    name: 'Chess In The Park Dev',
+                    value: 'youtube',
+                },
+                {
+                    name: 'Betrayal.io',
+                    value: 'betrayal',
+                },
+                {
+                    name: 'Fishington.io',
+                    value: 'fishington'
+                },
             ]
         },
     ],
-    async execute(client, interaction, args, Discord) {
-        
-            let channel = interaction.member.voice.channel;
-            if(!channel){
+    async execute({ client, interaction, args, Discord }) {
+
+        let channel = interaction.member.voice.channel;
+        if (!channel) {
+            const embed = new Discord.MessageEmbed()
+                .setAuthor(`You need to be in a voice channel for activities!`, interaction.user.displayAvatarURL({ dynamic: true }))
+            return interaction.followUp({ embeds: [embed] });
+        }
+
+        const selection = interaction.options.get("which")
+
+        let activities = {
+            yt: {
+                id: "880218394199220334",
+                name: "YouTube",
+                color: '#FF0000',
+            },
+            ytdev: {
+                id: "880218832743055411",
+                name: "YouTube Dev",
+                color: '#880000',
+            },
+            poker: {
+                id: "755827207812677713",
+                name: "Poker Night",
+                color: '#F700FF',
+            },
+            betrayal: {
+                id: "773336526917861400",
+                name: "Betrayal.io",
+                color: '#B86E00',
+            },
+            fishington: {
+                id: "814288819477020702",
+                name: "Fishington.io",
+                color: '#0099FF',
+            },
+            chess: {
+                id: "832012774040141894",
+                name: "Chess In The Park",
+                color: '#259E00',
+            },
+            chessdev: {
+                id: "832012586023256104",
+                name: "Chess In The Park Dev",
+                color: '#155C00',
+            },
+
+        }
+
+        let selectedactivity = activities[`${selection.value}`]
+
+        fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
+            method: "POST",
+            body: JSON.stringify({
+                max_age: 86400,
+                max_uses: 0,
+                target_application_id: `${selectedactivity.id}`,
+                target_type: 2,
+                temporary: false,
+                validate: null
+            }),
+            headers: {
+                "Authorization": `Bot ${LECHSBOTTKEY}`,
+                "Content-Type": "application/json"
+            }
+        }).then(res => res.json())
+            .then(async invite => {
+                if (!invite.code) {
+                    const embed = new Discord.MessageEmbed()
+                        .setAuthor(`Sorry but we cannot start ${selectedactivity.name}`, interaction.user.displayAvatarURL({ dynamic: true }))
+                        .setColor(`${selectedactivity.color}`)
+                    interaction.followUp({ embeds: [embed] });
+                }
+
+                const row = new Discord.MessageActionRow().addComponents(
+                    new Discord.MessageButton()
+                        .setStyle("LINK")
+                        .setLabel('Start Activity')
+                        .setURL(`https://discord.com/invite/${invite.code}`)
+                )
+
+
                 const embed = new Discord.MessageEmbed()
-                .setAuthor(`You need to be in a voice channel for activities!`, interaction.user.displayAvatarURL({dynamic: true}))
-                return interaction.followUp({ embeds: [embed] });
-            }
+                    .setTitle(`Click button to start ${selectedactivity.name}`)
+                    .setColor(`${selectedactivity.color}`)
+                interaction.followUp({ embeds: [embed], components: [row] })
 
-            const selection = interaction.options._hoistedOptions[0].value
 
-            if(selection === 'Youtube Together'){
-                fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
-                    method: "POST",
-                    body: JSON.stringify({
-                        max_age: 86400,
-                        max_uses: 0,
-                        target_application_id: "755600276941176913",
-                        target_type: 2,
-                        temporary: false,
-                        validate: null
-                    }),
-                    headers: {
-                        "Authorization": `Bot ${LECHSBOTTKEY}`,
-                        "Content-Type": "application/json"
-                    }
-                }).then(res => res.json())
-                .then(async invite =>{
-                    if(!invite.code){
-                        const embed = new Discord.MessageEmbed()
-                        .setAuthor(`Sorry but we cannot Click to start YouTube Together`, interaction.user.displayAvatarURL({dynamic: true}))
-                        .setColor('#FF0000')
-                        interaction.followUp({ embeds: [embed] });
-                    }
-    
-                    const row = new Discord.MessageActionRow().addComponents(
-                        new Discord.MessageButton()
-                        .setStyle("LINK")
-                        .setLabel('Click to start')
-                        .setEmoji("846030610526634005")
-                        .setURL(`https://discord.com/invite/${invite.code}`)
-                    )
-                    
-                    
-                    const embed = new Discord.MessageEmbed()
-                    .setTitle(`Click button to watch YouTube`)
-                    .setColor('#FF0000')
-                    interaction.followUp({ embeds: [embed], components: [row] })
-    
-                    
-                })
-            } else if(selection === 'Poker Night'){
-                fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
-                method: "POST",
-                body: JSON.stringify({
-                    max_age: 86400,
-                    max_uses: 0,
-                    target_application_id: "755827207812677713",
-                    target_type: 2,
-                    temporary: false,
-                    validate: null
-                }),
-                headers: {
-                    "Authorization": `Bot ${LECHSBOTTKEY}`,
-                    "Content-Type": "application/json"
-                }
-                }).then(res => res.json())
-                .then(async invite =>{
-                    if(!invite.code){
-                        const embed = new Discord.MessageEmbed()
-                        .setAuthor(`Sorry but we cannot Click to start Poker Night`, interaction.user.displayAvatarURL({dynamic: true}))
-                        .setColor('#FF00FF')
-                        interaction.followUp({ embeds: [embed] });
-                    }
+            })
 
-                    const row = new Discord.MessageActionRow().addComponents(
-                        new Discord.MessageButton()
-                        .setStyle("LINK")
-                        .setLabel('Click to start')
-                        .setEmoji("863549182635933697")
-                        .setURL(`https://discord.com/invite/${invite.code}`)
-                    )
 
-                    const embed = new Discord.MessageEmbed()
-                    .setTitle(`Click button to play the Poker Night`)
-                    .setColor('#FF00FF')
-                    interaction.followUp({ embeds: [embed], components: [row] })
-                    
-                })
-            } else if(selection === 'Betrayal.io'){
-                fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
-                method: "POST",
-                body: JSON.stringify({
-                    max_age: 86400,
-                    max_uses: 0,
-                    target_application_id: "773336526917861400",
-                    target_type: 2,
-                    temporary: false,
-                    validate: null
-                }),
-                headers: {
-                    "Authorization": `Bot ${LECHSBOTTKEY}`,
-                    "Content-Type": "application/json"
-                }
-                }).then(res => res.json())
-                .then(async invite =>{
-                    if(!invite.code){
-                        const embed = new Discord.MessageEmbed()
-                        .setAuthor(`Sorry but we cannot Click to start Betrayal.io`, interaction.user.displayAvatarURL({dynamic: true}))
-                        .setColor('#FFE600')
-                        interaction.followUp({ embeds: [embed] });
-                    }
-
-                    const row = new Discord.MessageActionRow().addComponents(
-                        new Discord.MessageButton()
-                        .setStyle("LINK")
-                        .setLabel('Click to start')
-                        .setEmoji("🔪")
-                        .setURL(`https://discord.com/invite/${invite.code}`)
-                    )
-
-                    const embed = new Discord.MessageEmbed()
-                    .setTitle(`Click button to play the Betrayal.io`)
-                    .setURL(`https://discord.com/invite/${invite.code}`)
-                    .setColor('#FFE600')
-                    interaction.followUp({ embeds: [embed], components: [row] })
-                })
-            } else if(selection === 'Fishington.io'){
-                fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
-                method: "POST",
-                body: JSON.stringify({
-                    max_age: 86400,
-                    max_uses: 0,
-                    target_application_id: "814288819477020702",
-                    target_type: 2,
-                    temporary: false,
-                    validate: null
-                }),
-                headers: {
-                    "Authorization": `Bot ${LECHSBOTTKEY}`,
-                    "Content-Type": "application/json"
-                }
-                }).then(res => res.json())
-                .then(async invite =>{
-                    if(!invite.code){
-                        const embed = new Discord.MessageEmbed()
-                        .setAuthor(`Sorry but we cannot Click to start Fishingtion.io`, interaction.user.displayAvatarURL({dynamic: true}))
-                        .setColor('#00C3FF')
-                        interaction.followUp({ embeds: [embed] });
-                    }
-
-                    const row = new Discord.MessageActionRow().addComponents(
-                        new Discord.MessageButton()
-                        .setStyle("LINK")
-                        .setLabel('Click to start')
-                        .setEmoji("🐟")
-                        .setURL(`https://discord.com/invite/${invite.code}`)
-                    )
-
-                    const embed = new Discord.MessageEmbed()
-                    .setTitle(`Click button to play the Fishingtion.io`)
-                    .setURL(`https://discord.com/invite/${invite.code}`)
-                    .setColor('#00C3FF')
-                    interaction.followUp({ embeds: [embed], components: [row] })
-                })
-            }
-        
-        
- }
-}
+    }
+})
